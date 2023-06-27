7b6cd3b5fab7015f8bed7ace7aa520f2662798be
sol <sadlthf@icloud.com>
2023-06-27 15:17:39 +0900

    #1 - 유저 엔티티 수정

--- src/main/java/com/ddam/spring/controller/MemberController.java
+++ src/main/java/com/ddam/spring/controller/MemberController.java
@@ -1,109 +1,33 @@
 package com.ddam.spring.controller;
 
-import java.util.List;
+import com.ddam.spring.domain.User;
+import com.ddam.spring.repository.UserRepository;
+import com.ddam.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.Validator;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.ddam.spring.domain.User;
-import com.ddam.spring.dto.UserFormDto;
-import com.ddam.spring.repository.UserRepository;
-import com.ddam.spring.service.UserService;
-import com.ddam.spring.validation.UserValidator;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RequestMapping("/members")
-@Controller
 @RequiredArgsConstructor
+@Controller
+@RequestMapping("/members")
 public class MemberController {
 
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-	public MemberController(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.userRepository = userRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-
-	// 회원가입 페이지
-	@GetMapping("/join")
-	public String join(Model model) {
-		model.addAttribute("UserFormDto", new UserFormDto());
-		return "members/join";
-	}
-
-	@PostMapping("/join")
-	public String joinOk(@Valid UserFormDto userFormDto, BindingResult bindingResult, Model model,
-			RedirectAttributes redirectAttributes) {
-
-		// Validator 생성
-		Validator validator = new UserValidator();
-		validator.validate(userFormDto, bindingResult);
-
-		// 유효성 검사
-		if (bindingResult.hasErrors()) {
-
-			redirectAttributes.addFlashAttribute("username", userFormDto.getUsername());
-			redirectAttributes.addFlashAttribute("passowrd", userFormDto.getPassword());
-			redirectAttributes.addFlashAttribute("name", userFormDto.getName());
-			redirectAttributes.addFlashAttribute("gender", userFormDto.getGender());
-			redirectAttributes.addFlashAttribute("email", userFormDto.getEmail());
-			redirectAttributes.addFlashAttribute("phone", userFormDto.getPhone());
-
-			return "members/join";
-		}
-
-		try {
-			User user = User.createUser(userFormDto, bCryptPasswordEncoder);
-			redirectAttributes.addFlashAttribute("message", "회원가입 성공!");
-			userService.saveUser(user);
-			System.out.println(user + " insert 성공");
-		} catch (IllegalStateException e) {
-			model.addAttribute("errorMessage", e.getMessage());
-			return "members/join";
-		}
-
-		return "redirect:/members/login";
-	}
-
-	// 로그인 페이지
-	@GetMapping(value = "/login")
-	public String loginMember() {
-		System.out.println("GET: /login");
-		System.out.println("");
-
-		return "/members/login";
-	}
+	private final UserService userService;
+	private final UserRepository userRepository;
 
 	@PostMapping(value = "/loginOk")
 	public String loginOk(@Validated @ModelAttribute User user, String username, String password,
@@ -157,26 +81,4 @@ public class MemberController {
 		}
 		return "redirect:/";
 	}
-
-	// 검증 실패 시 에러 메시지 출력
-	public void showErrors(Errors errors, RedirectAttributes redirectAttributes) {
-		if (errors.hasErrors()) {
-			List<FieldError> errorList = errors.getFieldErrors();
-
-			for (FieldError error : errorList) {
-				String code = error.getCode();
-
-				if (code.equals("emptyName")) {
-					redirectAttributes.addFlashAttribute("nameError", "※ 이름을 입력해주세요");
-				} else if (code.equals("emptyUsername")) {
-					redirectAttributes.addFlashAttribute("usernameError", "※ 닉네임을 입력해주세요");
-				} else if (code.equals("emptyPassword")) {
-					redirectAttributes.addFlashAttribute("passwordError", "※ 비밀번호를 입력해주세요");
-				} else if (code.equals("emptyEmail")) {
-					redirectAttributes.addFlashAttribute("emialError", "※ 이메일을 입력해주세요");
-				}
-			}
-		}
-	}
-
 }
--- src/main/java/com/ddam/spring/service/UserService.java
+++ src/main/java/com/ddam/spring/service/UserService.java
@@ -1,74 +1,27 @@
 package com.ddam.spring.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import com.ddam.spring.domain.User;
+import com.ddam.spring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ddam.spring.domain.User;
-import com.ddam.spring.repository.UserRepository;
+import java.util.Optional;
 
-import lombok.AllArgsConstructor;
-
-@EnableAutoConfiguration
-@AllArgsConstructor
-@Service("userDetailsService")  // 빈 등록하기
-public class UserService implements UserDetailsService{
+@RequiredArgsConstructor
+@Service
+public class UserService{
 	
 	private final UserRepository userRepository;
-	
-	
-	
+
 	@Transactional
 	public User saveUser(User user){
-
-        validateDuplicateMember(user);
-//        String hashPw = bCryptPasswordEncoder.encode(user.getPassword());
-//        user.setPassword(hashPw);
-
-       return userRepository.save(user);
-        
-    }
-	
-	private void validateDuplicateMember(User user){
-		User findUser = userRepository.findByUsername(user.getUsername());
-        if(findUser != null){
-            throw new IllegalStateException("이미 가입된 회원입니다.");
-        }
+        return userRepository.save(user);
     }
 
-	public User loadUserByUsername(String username) throws UsernameNotFoundException {
+	public Optional<User> findByUsername(String username) throws UsernameNotFoundException {
 
-		User user = userRepository.findByUsername(username);
-
-        if(user == null){
-            throw new UsernameNotFoundException(username);
-        }
-        
-        List<GrantedAuthority> roles = new ArrayList<>();
-//        roles.add(new SimpleGrantedAuthority(user.getRole()));
-//
-//        UserContext userContext = new UserContext(user,roles);
-
-        return User.builder()
-        		.id(user.getId()) // 이영진!!!!!!!! by 강사.
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .name(user.getName())
-                .gender(user.getGender())
-                .email(user.getEmail())
-                .phone(user.getPhone())
-                .build();
+        return userRepository.findByUsername(username);
     }
-	
-
 }
